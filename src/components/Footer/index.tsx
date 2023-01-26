53bc73de5e541fd249864f76b8fa8f705ef3b6d1
Will <williansilva3dsi@hotmail.com>
2023-01-26 19:06:49 -0300

    feat: footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+const Frooter = () => (
+  <Container>
+    <span>MKS sistemas © Todos os direitos reservados</span>
+  </Container>
+)
+
+export default Frooter;
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Container = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 34px;
+  background: #eeeeee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > span {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+  }
+`;
+
+export { Container };
