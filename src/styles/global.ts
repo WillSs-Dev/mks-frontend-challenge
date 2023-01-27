38a77984b7f1ec962be41ca0c17089aa595ff798
Will <williansilva3dsi@hotmail.com>
2023-01-27 14:41:52 -0300

    refactor: removing font impor on global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap');
 
   * {
     margin: 0;
@@ -10,7 +9,6 @@ const GlobalStyle = createGlobalStyle`
     }
   body {
     background: #E5E5E5;
-    min-height: 100vH;
 
     > #root {
       min-height: 100vH;
