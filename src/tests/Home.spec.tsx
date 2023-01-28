f246cd1b43114d196dcf385e8682663667f7bbcd
Will <williansilva3dsi@hotmail.com>
2023-01-28 18:35:28 -0300

    fix: extending jest's the expect method of assertion

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
 
 import * as productsAPI from '../API/products';
 import App from '../App';
