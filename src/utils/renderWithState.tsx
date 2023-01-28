5d26ccfaf1b24ad7a9bf649d5bd6c12264b7d85d
Will <williansilva3dsi@hotmail.com>
2023-01-28 11:11:26 -0300

    fix: typing of App component inside function parameters

--- src/utils/renderWithState.tsx
+++ src/utils/renderWithState.tsx
@@ -1,9 +1,10 @@
 import { render } from '@testing-library/react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
 import { store } from '../store';
 
-const renderWithState = (App: React.FC) => {
+const renderWithState = (App: () => JSX.Element) => {
   return render(
     <Provider store={store}>
       <App />
