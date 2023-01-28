d748975261111d7418f35726eb72d376137cba3e
Will <williansilva3dsi@hotmail.com>
2023-01-28 16:13:47 -0300

    refactor: routes inside the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Home from './pages/Home';
 import GlobalStyles from './styles/global';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
 
 const App = () => (
   <>
     <GlobalStyles />
-    <Home />
+    <RouterProvider router={router} />
   </>
 );
 
