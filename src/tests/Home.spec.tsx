187c68ad973a23715d872b817b494140d49ebefd
Will <williansilva3dsi@hotmail.com>
2023-01-28 12:47:14 -0300

    refactor: assertion position on test body

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -22,11 +22,11 @@ describe('Assertions on initial page load', () => {
 
   it('Renders the app', async () => {
     renderWithState(App);
-    await expect(productsAPI.getProducts()).resolves.toEqual(mockProducts);
-
+    
     const cards = await waitFor(() => screen.getAllByTestId('product-card'));
     const cardButtons = await waitFor(() => screen.getAllByTestId('add-to-cart'));
-
+    
+    await expect(productsAPI.getProducts()).resolves.toEqual(mockProducts);
     expect(cards.length).toBe(5);
     expect(cardButtons.length).toBe(5);
   });
