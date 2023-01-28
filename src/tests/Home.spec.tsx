281ddc51fd67ea0ad0bf756201cd153f4e7a4a53
Will <williansilva3dsi@hotmail.com>
2023-01-28 13:08:25 -0300

    feat: testing cart sidebar contents and open/close interactions

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -1,4 +1,5 @@
 import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import * as productsAPI from '../API/products';
 import App from '../App';
@@ -29,5 +30,35 @@ describe('Assertions on initial page load', () => {
     await expect(productsAPI.getProducts()).resolves.toEqual(mockProducts);
     expect(cards.length).toBe(5);
     expect(cardButtons.length).toBe(5);
+  });
+
+  it('The cart sidebar opens and closes', async () => {
+    renderWithState(App);
+
+    const cartButton = await waitFor(() => screen.getByTestId('cart-button'));
+    const cartSidebar = await waitFor(() => screen.getByTestId('cart-sidebar'));
+
+    // OPEN
+    userEvent.click(cartButton);
+    await waitFor(() => expect(cartSidebar).toHaveStyle('min-height: 100%'));
+
+    const closeButton = await waitFor(() => screen.getByTestId('close-button'));
+
+    // CLOSE
+    userEvent.click(closeButton);
+    await waitFor(() => expect(cartSidebar).toHaveStyle('min-height: 0'));
+  });
+
+  it('Renders the app, inittialy with no products on the cart', async () => {
+    renderWithState(App);
+
+    const cartButton = await waitFor(() => screen.getByTestId('cart-button'));
+    const cartSidebar = await waitFor(() => screen.getByTestId('cart-sidebar'));
+
+    userEvent.click(cartButton);
+    await waitFor(() => expect(cartSidebar).toHaveContent('Nada no carrinho!'));
+
+    const checkoutTotalValue = await waitFor(() => screen.getByTestId('checkout-total-value'));
+    expect(checkoutTotalValue).toHaveTextContent('0');
   });
 });
