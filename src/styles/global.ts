6cbf374ea9a85ecd774996bff1fee106508a04dc
Will <williansilva3dsi@hotmail.com>
2023-01-29 09:52:00 -0300

    feat: custom scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -9,6 +9,19 @@ const GlobalStyle = createGlobalStyle`
     }
   body {
     background: #E5E5E5;
+
+    ::-webkit-scrollbar {
+      width: 5px;
+      border-radius: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: #373737;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1; 
+    }
 
     > #root {
       min-height: 100vH;
